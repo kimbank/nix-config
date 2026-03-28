49a93d965185273b541eadaddb52d17ca10988c2
kimbank <kimeunhang@proton.me>
2026-03-28 17:27:42 +0900

    feat: add oh-my-zsh configuration and custom shell aliases

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -11,6 +11,12 @@ in
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    oh-my-zsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins = [ "git" ];
+    };
+
     initContent = lib.mkBefore ''
       if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
@@ -24,6 +30,14 @@ in
     shellAliases = {
       ll = "ls -lah";
       cls = "clear";
+      "친" = "clear";
+      "ㅊㅣㄴ" = "clear";
+      "히" = "gl";
+      "ㅎㅣ" = "gl";
+      wd = "while true; do tput cup 0 0; command duf; sleep 2; done";
+      dp = "watch -n 1 \"docker ps -a --format \\\"table {{.ID}}\\t{{.Names}}\\t{{.Status}}\\\"\"";
+      we = "webstorm .";
+      clauded = "claude --dangerously-skip-permissions";
     };
   };
 
