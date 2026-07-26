9da89b8f3e104d3efb5c2b25cceef03418415c79
kimbank <kimeunhang@proton.me>
2026-07-26 13:18:23 +0900

    feat: add custom alias for pnpm global package update

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -98,6 +98,7 @@ in
       "ㅣㅣ" = "ll";
       "ㄷ턋" = "exit";
       "exot" = "exit";
+      pug = "pnpm_config_minimum_release_age=0 pnpm up -g";
       wd = "while true; do tput cup 0 0; command duf; sleep 2; done";
       dp = "watch -n 1 \"docker ps -a --format \\\"table {{.ID}}\\t{{.Names}}\\t{{.Status}}\\\"\"";
       we = "webstorm .";
